7bd4063252a200a510904ebf0378c3e3361b37e9
Анна <a.marshalova@g.nsu.ru>
2023-12-20 22:00:30 +0700

    edit formatting

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,28 +1,34 @@
-import os
-import sys
 import json
+import os
 import random
+
 from IPython.display import clear_output
 
-sys.path.append('src')
-from utils import load_or_create_json
+from src.utils import load_or_create_json
 
 
 class Evaluator:
-    def __init__(self, report_path='evaluation/report.json', eval_dir='evaluation/results',
-                 guidelines_path='evaluation/guidelines.json', urls_path='evaluation/urls.json', shuffle=True):
-        self.annotator = input('Enter your name')
+    def __init__(
+            self,
+            report_path="evaluation/report.json",
+            eval_dir="evaluation/results",
+            guidelines_path="evaluation/guidelines.json",
+            urls_path="evaluation/urls.json",
+            shuffle=True,
+    ):
+        self.annotator = input("Enter your name")
         self.report_path = report_path
-        self.eval_path = os.path.join(eval_dir, f'{self.annotator}_eval.json')
+        self.eval_path = os.path.join(eval_dir, f"{self.annotator}_eval.json")
         self.reports = load_or_create_json(self.report_path)
         self.evaluated = load_or_create_json(self.eval_path)
-        self.CRITERIA = {'adequacy': (0, 5),
-                         'reliability': (0, 5),
-                         'usefulness': (0, 5),
-                         'honesty': (0, 5),
-                         'linguistic correctness': (0, 5),
-                         'coherence': (0, 5)
-                         }
+        self.CRITERIA = {
+            "adequacy": (0, 5),
+            "reliability": (0, 5),
+            "usefulness": (0, 5),
+            "honesty": (0, 5),
+            "linguistic correctness": (0, 5),
+            "coherence": (0, 5),
+        }
         with open(guidelines_path) as jf:
             self.guidelines = json.load(jf)
         with open(urls_path) as jf:
@@ -35,25 +41,26 @@ class Evaluator:
 
         print(f"CV: {report['cv']}\nVacancy:{report['job']}\n")
 
-        guideline = self.guidelines.get(f"{report['cv']} & {report['job']}", '')
-        cv_url = self.urls.get(report['cv'])
-        vacancy_url = self.urls.get(report['job'])
+        guideline = self.guidelines.get(f"{report['cv']} & {report['job']}",
+                                        "")
+        cv_url = self.urls.get(report["cv"])
+        vacancy_url = self.urls.get(report["job"])
         if cv_url:
-            print(f'CV URL: {cv_url}')
+            print(f"CV URL: {cv_url}")
         if vacancy_url:
-            print(f'VACANCY URL: {vacancy_url}')
+            print(f"VACANCY URL: {vacancy_url}")
 
         if guideline:
             print(f"guidelines: {guideline}\n")
 
-        print(report['output'])
+        print(report["output"])
 
         scores = {}
         for criterion, (min_score, max_score) in self.CRITERIA.items():
-            score = int(input(f'{criterion} ({min_score}-{max_score})'))
+            score = int(input(f"{criterion} ({min_score}-{max_score})"))
             scores.update({criterion: score})
         self.evaluated.update({report_id: {**report, **scores}})
-        with open(self.eval_path, 'w') as jf:
+        with open(self.eval_path, "w") as jf:
             json.dump(self.evaluated, jf)
 
     def run(self):
@@ -61,6 +68,6 @@ class Evaluator:
         if self.shuffle:
             random.shuffle(report_ids)
         for report_id in report_ids:
-            if not report_id in self.evaluated:
+            if report_id not in self.evaluated:
                 self.evaluate_report(report_id)
-        print('All texts were evaluated. Thank you!')
+        print("All texts were evaluated. Thank you!")
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,16 @@
+import gc
+import json
 import os
 import re
-import json
+
+import torch
 
 
 def parse_matches(path):
-    SEP = '*****'
-    VACANCY_SEP = '***** vacansy'
-    guidelines_RE = re.compile('Оценка \d/\d+:.*')
-    URL_RE = re.compile('\(https://.*\)')
+    SEP = "*****"
+    VACANCY_SEP = "***** vacansy"
+    guidelines_RE = re.compile(r"Оценка \d/\d+:.*")
+    URL_RE = re.compile(r"\(https://.*\)")
     with open(path) as f:
         lines = f.readlines()
     cv = []
@@ -17,7 +20,7 @@ def parse_matches(path):
         i += 1
         if i == len(lines):
             break
-    cv = ''.join(cv)
+    cv = "".join(cv)
 
     vacancies = []
     guidelines = []
@@ -26,15 +29,15 @@ def parse_matches(path):
         if line.startswith(VACANCY_SEP):
             vacancies.append([])
             url = URL_RE.findall(line)
-            url.append('')
+            url.append("")
             urls.append(url[0])
 
         elif line.startswith(SEP):
             guidelines.append(guidelines_RE.findall(line))
         else:
             vacancies[-1].append(line)
-    vacancies = [''.join(vacancy) for vacancy in vacancies]
-    guidelines = [' '.join(guideline) for guideline in guidelines]
+    vacancies = ["".join(vacancy) for vacancy in vacancies]
+    guidelines = [" ".join(guideline) for guideline in guidelines]
     assert len(vacancies) == len(guidelines)
     return cv, vacancies, guidelines, urls
 
@@ -47,29 +50,33 @@ def load_or_create_json(json_path):
     return d
 
 
-def save_matches(job,
-                 matches_dir='LLMmatch/data/matches',
-                 guidelines_path='evaluation/guidelines.json',
-                 urls_path='evaluation/urls.json'):
+def save_matches(
+        job,
+        matches_dir="LLMmatch/data/matches",
+        guidelines_path="evaluation/guidelines.json",
+        urls_path="evaluation/urls.json",
+):
     guideline_dict = load_or_create_json(guidelines_path)
     urls_dict = load_or_create_json(urls_path)
-    matches_path = os.path.join(matches_dir, f'{job}.txt')
+    matches_path = os.path.join(matches_dir, f"{job}.txt")
     cv, vacancies, guidelines, urls = parse_matches(matches_path)
-    cv_path = os.path.join('data', 'CVs', f'{job}.txt')
-    vacancy_dir = os.path.join('data', 'Vacancies', job)
+    cv_path = os.path.join("data", "CVs", f"{job}.txt")
+    vacancy_dir = os.path.join("data", "Vacancies", job)
     os.makedirs(vacancy_dir, exist_ok=True)
-    with open(cv_path, 'w') as f:
+    with open(cv_path, "w") as f:
         f.write(cv)
-    for i, (vacancy, guideline, url) in enumerate(list(zip(vacancies, guidelines, urls)), start=1):
-        vacancy_path = os.path.join(vacancy_dir, f'{i}.txt')
-        with open(vacancy_path, 'w') as f:
+    for i, (vacancy, guideline, url) in enumerate(
+            list(zip(vacancies, guidelines, urls)), start=1
+    ):
+        vacancy_path = os.path.join(vacancy_dir, f"{i}.txt")
+        with open(vacancy_path, "w") as f:
             f.write(vacancy)
         guideline_dict.update({f"{cv_path} & {vacancy_path}": guideline})
         if url:
             urls_dict.update({vacancy_path: url})
-    with open(guidelines_path, 'w') as jf:
+    with open(guidelines_path, "w") as jf:
         json.dump(guideline_dict, jf, ensure_ascii=False)
-    with open(urls_path, 'w') as jf:
+    with open(urls_path, "w") as jf:
         json.dump(urls_dict, jf, ensure_ascii=False)
 
 
